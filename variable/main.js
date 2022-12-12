a76a53dadd191df4b2bea04c47512347fceef8f0
Elsakrishi29 <112686658+Elsakrishi29@users.noreply.github.com>
2022-12-12 11:38:54 +0000

    Add files via upload

---
+++ variable/main.js
@@ -0,0 +1,80 @@
+                             
+                             /*variables*/
+var num=5;
+    console.log(num);
+    console.log(typeof (num));
+
+var fname="krishna";
+    console.log(fname);
+    console.log(typeof (fname));
+
+var lName="veni";
+    console.log(lName);
+    console.log(typeof (lName));    
+
+var LName="veni";
+    console.log(LName);
+    console.log(typeof (LName));    
+
+var yes=true;
+var no=false;    
+    console.log(yes);
+    console.log(typeof (yes)); 
+    console.log(no);
+    console.log(typeof (no)); 
+
+var define;
+    console.log(define);   
+    
+var defined= null;   
+    console.log(defined);
+    console.log(typeof (defined)); 
+ 
+var defines="null";
+    console.log(defines);
+
+var value=1234567890;
+    console.log(value); 
+    console.log(typeof (value));  
+
+var values=BigInt(1234567890);
+    console.log(values);
+    console.log(typeof (values)); 
+   
+var val=(5n);
+    console.log(val);
+    console.log(typeof (val));
+    
+var octNum=0255;
+var hexNum=0XEE;
+    console.log(octNum);
+    console.log(hexNum);  
+    
+var Num=0255;
+var Num=0XEE;
+    console.log(Num);
+    console.log(Num);
+    console.log(typeof (Num));
+    console.log(typeof (Num));   
+
+                            /*let*/
+let firstname="krishna"
+   console.log(firstname);
+
+
+        /*preincreament and postincreament*/
+var a=5;
+   console.log(++a);
+var b=5;
+   console.log(b++);
+   console.log(b);
+
+   var age=6;
+    var scladmion=(age>=5)?'eligible':'noteligible';
+    console.log(scladmion);
+   
+    
+
+
+
+  
